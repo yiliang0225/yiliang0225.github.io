639e5d5907214e0f674e466591d87f625d654b09
liang <3029613984@qq.com>
2022-03-10 20:57:42 +0800

    Site updated: 2022-03-10 20:57:41

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-        document.title = '我相信你还会回来的！';
+        document.title = '拜拜！';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-        document.title = '哈哈，我就知道！' + OriginTitle;
+        document.title = '哈哈哈！' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
